cf553fb18666093fe43745c97a2e2c27e0ecd6d2
In_Hoi <76868046+inhoi@users.noreply.github.com>
2024-10-05 15:01:37 -0400

    Add CORS

--- app/src/app.py
+++ app/src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from api.routes import api
 
 app = Flask(__name__)
+CORS(app) #To enbale for all domains on all routes
 app.register_blueprint(api)
 
 @app.route('/')
