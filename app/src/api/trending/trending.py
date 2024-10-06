75a549f109ff93419b7816eafd2170c5dee7a601
joyinning <joyinning1@gmail.com>
2024-10-06 19:55:52 -0400

    Add search routes (3-1), revise arguments in trending (use user_input)

--- app/src/api/trending/trending.py
+++ app/src/api/trending/trending.py
@@ -1,16 +1,18 @@
 from datetime import datetime
+from . import user_input
+from .. import dummy_restaurants
 
 class TrendingRestaurants:
     """
     logic to fetch and process trending restaurants
     """
 
-    def __init__(self, zipcode=None, restaurants=[]):
-        self.zipcode = zipcode
-        self.restaurants = restaurants
+    def __init__(self, user_input):
+        self.zipcode = user_input.get("zipcode")
+        self.restaurants = [] 
 
     def get_trending_restaurants(self):
-        trending = sorted(self.restaurants, key=lambda x: (-x['rating'], x['id']))
+        trending = sorted(dummy_restaurants, key=lambda x: (-x['rating'], x['id']))
         output = [{
             "name": restaurant["name"], 
             "avg_review": restaurant["rating"],
--- app/src/app.py
+++ app/src/app.py
@@ -7,6 +7,7 @@ from api.personalization_routes import personalization_bp
 from api.trending_routes import trending_bp
 from api.recommender_routes import collaborative_based_bp
 from api.recommender_routes import content_based_bp
+from api.search_routes import search_bar_bp
 app = Flask(__name__)
 CORS(app) #To enbale for all domains on all routes
 # Register the blueprint
@@ -17,6 +18,7 @@ app.register_blueprint(personalization_bp)
 app.register_blueprint(trending_bp)
 app.register_blueprint(collaborative_based_bp)
 app.register_blueprint(content_based_bp)
+app.register_blueprint(search_bar_bp)
 
 
 
