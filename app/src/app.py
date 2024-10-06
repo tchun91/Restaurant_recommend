4256e582843b1ed03ab649f8c96e90f9159b6f8a
joyinning <joyinning1@gmail.com>
2024-10-06 19:19:30 -0400

    Add trending blueprint in app.py (1-1)

--- app/src/app.py
+++ app/src/app.py
@@ -4,6 +4,7 @@ from api.routes import api_bp
 from api.recommender_routes import recommendation_bp
 from api.chatbot_routes import chatbot_bp
 from api.personalization_routes import personalization_bp
+from api.trending_routes import trending_bp
 app = Flask(__name__)
 CORS(app) #To enbale for all domains on all routes
 # Register the blueprint
@@ -11,6 +12,7 @@ app.register_blueprint(api_bp)
 app.register_blueprint(recommendation_bp)
 app.register_blueprint(chatbot_bp)
 app.register_blueprint(personalization_bp)
+app.register_blueprint(trending_bp)
 
 
 
